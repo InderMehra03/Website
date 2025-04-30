cffdfe7901aed1dfd454e0c6335457fccf80d40c
InderMehra03 <inder.mehra.2026@gmail.com>
2025-04-30 23:44:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const text = document.getElementById("textToConvert");
+const convertBtn = document.getElementById("convertBtn");
+
+convertBtn.addEventListener('click', function () {
+    const speechSynth = window.speechSynthesis;
+    const enteredText = text.value.trim();
+    const error = document.querySelector('.error-para');
+
+    if (!enteredText) {
+        error.textContent = "Nothing to Convert! Enter text in the text area.";
+        return;
+    }
+
+    if (!speechSynth.speaking) {
+        error.textContent = "";
+        const newUtter = new SpeechSynthesisUtterance(enteredText);
+        speechSynth.speak(newUtter);
+        convertBtn.textContent = "Sound is Playing...";
+
+        newUtter.onend = () => {
+            convertBtn.textContent = "Play Converted Sound";
+        };
+    }
+});
